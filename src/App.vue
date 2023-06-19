75c6c8577593289c116adcd9a2b322134944b6b5
Admin <roman.nikolaevich@gmail.com>
2023-06-19 21:07:35 +0300

    creating a page with adding new messages, as well as displaying them on the page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-    <div>
-        <div>
-            <button @click="addLike">Like</button>
-            <button @click="addDislike">Dislike</button>
-        </div>
-        <div>
-            Количество лайков: <strong> {{ likes }} </strong>
-        </div>
-        <div>
-            Количество дизлайков: <strong> {{ dislikes }} </strong>
+    <div class="app">
+        <form @submit.prevent>
+<!--            @submit.prevent - отключает действия браузера по умолчанию-->
+            <h4>Создание поста </h4>
+            <!--            v-bind - связывает данные с конкретным компонентом-->
+            <input v-bind:value="title"
+                   @input="title = $event.target.value"
+                   class="input"
+                   type="text"
+                   placeholder="Название">
+            <input v-bind:value="body"
+                   @input="body = $event.target.value"
+                   class="input"
+                   type="text"
+                   placeholder="Описание">
+            <button class="btn"
+                    @click="createPost"
+                    type="submit">
+                Создать
+            </button>
+        </form>
+        <div class="post" v-for="post in posts">
+            <div><strong>Название: </strong> {{ post.title }}</div>
+            <div><strong>Описание: </strong> {{ post.body }}</div>
         </div>
     </div>
 </template>
@@ -17,21 +31,69 @@
 export default {
     data() {
         return {
-            likes: 0,
-            dislikes: 0,
+            posts: [
+                {id: 1, title: 'Javascript 1', body: 'Описание поста 1'},
+                {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+                {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
+                {id: 4, title: 'Javascript 4', body: 'Описание поста 4'},
+            ],
+            title: '',
+            body: '',
         }
     },
     methods: {
-        addLike() {
-            this.likes += 1;
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            // отправляем данные в массив "posts" и очищаем поля input от введенных данных:
+            this.posts.push(newPost);
+            this.title = '';
+            this.body = '';
         },
-        addDislike() {
-            this.dislikes += 1;
-        }
+
     }
 }
 </script>
 
 <style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
+.app {
+    padding: 20px;
+}
+
+.post {
+    padding: 15px;
+    border: 1px solid teal;
+    margin-top: 15px;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+
+}
 </style>
