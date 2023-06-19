c0106f18adc897c876257e051cb0ccbf00a9949a
Admin <roman.nikolaevich@gmail.com>
2023-06-19 22:56:36 +0300

    Postlist to PostItem decomposition

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,15 +1,21 @@
 <template>
-    <div class="post" v-for="post in posts">
-        <div><strong>Название: </strong> {{ post.title }}</div>
-        <div><strong>Описание: </strong> {{ post.body }}</div>
+    <div>
+        <h3>Список пользователей</h3>
+        <post-item
+                v-for="post in posts"
+                :post="post"
+        />
     </div>
 </template>
 
 <script>
+import PostItem from "@/components/PostItem.vue";
+
 export default {
+    components: {PostItem},
     props: {
         posts: {
-           type: Array, //тип поста
+            type: Array, //тип поста
             required: true //делаем поле обязательным
         }
     }
