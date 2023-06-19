488d05cbebe22540c0f466b9957fb0db219e923d
Admin <roman.nikolaevich@gmail.com>
2023-06-19 22:02:00 +0300

    decomposition

---
+++ src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="post" v-for="post in posts">
+        <div><strong>Название: </strong> {{ post.title }}</div>
+        <div><strong>Описание: </strong> {{ post.body }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        posts: {
+           type: Array, //тип поста
+            required: true //делаем поле обязательным
+        }
+    }
+}
+</script>
+
+<style scoped>
+.post {
+    padding: 15px;
+    border: 1px solid teal;
+    margin-top: 15px;
+}
+</style>
