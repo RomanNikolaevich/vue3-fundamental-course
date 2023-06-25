0d2ff570c94c69727bff506e3db8ee85e4b55a40
Admin <roman.nikolaevich@gmail.com>
2023-06-25 23:57:48 +0300

    add mixins

---
+++ src/components/mixins/toggleMixin.js
@@ -0,0 +1,14 @@
+export default {
+    props: {
+        show: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        hideDialog() {
+            this.$emit('update:show', false)
+        }
+    }
+}
+// Вынесли сюда данные из модального окна MyDialog.vue
